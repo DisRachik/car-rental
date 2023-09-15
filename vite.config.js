12c9927f4279c14938738fcde12139794a4eda02
DisRachik <radchenkoig@gmail.com>
2023-09-16 00:06:44 +0300

    feat: fix vite.config for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   base: '/car-rental/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+    },
+  },
 });
