e5f831ec27b79874b840441b32ef028084495d4d
DisRachik <DisRachik@users.noreply.github.com>
2023-09-18 04:00:14 +0000

    deploy: c39a2245b1b39073c6f7625ca370b3aaed8816b7

--- assets/Home-7c2301f6.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-0116a5de.js";const o="/car-rental/vite.svg",e=()=>t.jsx("div",{children:t.jsx("img",{src:o,alt:"React logo"})});export{e as default};
---
+++ assets/Home-91a16108.js
@@ -0,0 +1,1 @@
+import{j as o,v as s}from"./index-85a74fa5.js";const r=()=>o.jsx("div",{children:o.jsx("img",{src:s,alt:"Car logo"})});export{r as default};
